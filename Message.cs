485cf1f417352e707084cb3ead4723fc5ff0f4ef
Dimitri Vanhaecke <dimitri.vanhaecke@student.hogent.be>
2021-01-02 01:06:51 +0200

    contact form wait anim

---
+++ Message.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MyProfiles
+{
+    public class Message
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string TextMessage { get; set; }
+
+        public void Send()
+        {
+        }
+    }
+}

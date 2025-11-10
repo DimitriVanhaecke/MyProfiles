b553d8928fa268db5358b8ea897111348e022d6f
dimitri.vanhaecke <dimitri.vanhaecke@buyck.be>
2025-11-10 11:04:14 +0100

    Update .Net 8

--- Program.cs
+++ Program.cs
@@ -1,7 +1,6 @@
+using MyProfiles;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using MyProfiles;
-
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
